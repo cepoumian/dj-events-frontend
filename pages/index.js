779f53d903006c2ddb6baaa2d79e81ba0823cff4
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2021-05-31 20:39:31 -0600

    Setup events and single event pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcomig Events</h1>
+      {events.length === 0 ? <h3>No events to show</h3> : null}
+
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
+      {events.length > 0 ? (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      ) : null}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  }
 }
